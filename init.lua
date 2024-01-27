10cbad817299ce3697bd0ec02c15ddbde4b2d680
Gabriel Do Couto Santos <gabrieldocoutosantos@bob.local>
2024-01-27 07:53:02 -0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+ 
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+vim.wo.relativenumber = true
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins")
+
+
+
+
+-- local builtin = require('telescope.builtin')
+
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
